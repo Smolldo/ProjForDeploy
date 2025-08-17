50c96d70d36175bc58c44718993e91d48d77becc
Mykhajlo Honyk <maximqwerbich@gmail.com>
2025-08-17 10:47:02 +0300

    1

--- project_db.py
+++ project_db.py
@@ -1,29 +1,36 @@
-from sqlalchemy import String, Float
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
-from sqlalchemy import create_engine
+import os
+from sqlalchemy import create_engine, String, Integer, ForeignKey, Column
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, Mapped, mapped_column
 
-PGUSER = 'postgres'
-PGPASSWORD = 'misha99'
 
-engine = create_engine(f"postgresql+psycopg2://{PGUSER}:{PGPASSWORD}@localhost:5432/web_convertor", echo=True)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+pg8000://")
+
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+
 Session = sessionmaker(bind=engine)
 
-class Base(DeclarativeBase):
-    def create_db(self):
-        Base.metadata.create_all(engine)
 
-    def drop_db(self):
-        Base.metadata.drop_all(engine)
+class Base(DeclarativeBase):
+    pass
+
 
 
 class Conversion(Base):
-    __tablename__ = 'conversions'
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    from_currency: Mapped[str] = mapped_column(String(3))
-    to_currency: Mapped[str] = mapped_column(String(3))
+    __tablename__ = "conversions"
 
-    amount: Mapped[float] = mapped_column(Float)
-    result: Mapped[float] = mapped_column(Float)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    input_value: Mapped[str] = mapped_column(String, nullable=False)
+    output_value: Mapped[str] = mapped_column(String, nullable=False)
 
-base = Base()
-base.create_db()
+
+
+def create_db():
+    Base.metadata.create_all(engine)
+
+def drop_db():
+    Base.metadata.drop_all(engine)
